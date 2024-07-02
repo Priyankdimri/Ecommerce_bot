1fca585c3d960d6e74b58fb243b0247ba786a233
Priyankdimri <114757150+Priyankdimri@users.noreply.github.com>
2024-07-02 17:15:25 +0530

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup( 
+	name='ecommerce_chatbot', 
+	version='0.1', 
+	description='Ecommerce chatbot on flipkart data', 
+	author='Priynk Dimri', 
+	packages=[''],
+    install_requirements=[] 
+	) 
